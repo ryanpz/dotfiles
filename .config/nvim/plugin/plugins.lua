a68f3643983c099a5a563943230c52a71c87d903
Ryan Perez <ryan@rypz.io>
2025-09-12 18:29:45 -0700

    nvim: ignore .git dir when fuzzy finder uses ripgrep

--- .config/nvim/plugin/plugins.lua
+++ .config/nvim/plugin/plugins.lua
@@ -18,7 +18,7 @@ local project_files = function()
   local files = {}
 
   local cmds = {
-    { 'rg', '--files', '--hidden', '--no-require-git', '--glob', '!.jj/' },
+    { 'rg', '--files', '--hidden', '--no-require-git', '--glob', '!.jj/', '--glob', '!.git/' },
     { 'git', 'ls-files', '--cached', '--others', '--exclude-standard', '.' },
     { 'jj', 'file', 'list', '.' },
     { 'find', '.', '-type', 'f' },
